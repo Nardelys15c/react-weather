5f51dce5306cb7849e99a5fe4f97de1b9c941b9e
Nardelys15c <136908202+Nardelys15c@users.noreply.github.com>
2024-05-05 13:58:49 -0400

    added opensource page

--- src/App.js
+++ src/App.js
@@ -3,8 +3,18 @@ import './App.css';
 
 export default function App() {
   return (
-    <div className="App"> Weather app 
+    <div className="App"> 
+    <h1>Weather app</h1>  
+    <footer>
+      This project was coded by Nardelys Caraballo and is {""}
+       <a href="https://github.com/Nardelys15c/react-weather.git"
+       target="_blank" rel="noreferrer">  
+       open-sourced in GitHub
+       </a>
        
+       </footer>
+
+
     </div>
   );
 }

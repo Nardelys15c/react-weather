3fd141ad57d6da050aeba415342e68512c7b1db6
Nardelys15c <136908202+Nardelys15c@users.noreply.github.com>
2024-05-11 18:30:41 -0400

    added github links

--- src/App.js
+++ src/App.js
@@ -2,14 +2,15 @@ import React from 'react';
 import './App.css';
 
 export default function App() {
-  return 
+  return( 
     <div className="App">
      <h1> Weather App</h1>
      <footer>
-      This is coded by Nardelys Caraballo and is 
-      <a href="/">open-sourced in Github</a>
+      This project was coded by Nardelys Caraballo and is   
+      <a href="https://github.com/Nardelys15c/react-weather.git" target="_blank" rel="noreferrer"> open-sourced in Github
+      </a>
      </footer>
      
     </div>
-  ;
+);
 }

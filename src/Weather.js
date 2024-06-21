2993f4c0c3e64d7e0231abf9ffdd8a23fa38e5fb
Nardelys15c <136908202+Nardelys15c@users.noreply.github.com>
2024-06-21 17:05:47 -0400

    added new weather component, fixed css and added a new useState

--- src/Weather.js
+++ src/Weather.js
@@ -17,7 +17,7 @@ const [city, setCity]=useState(props.defaultCity);
         humidity: response.data.main.humidity,
         date: new Date(response.data.dt * 1000),
         description:response.data.weather[0].description,
-        iconUrl:`https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
+        icon: response.data.weather[0].icon,
         city: response.data.name
     });
  
--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -1,5 +1,6 @@
 import React from "react";
 import FormattedDate from "./FormattedDate";
+import WeatherIcon from "./WeatherIcon";
 
 export default function WeatherInfo(props){
     return(
@@ -11,11 +12,15 @@ export default function WeatherInfo(props){
          </li>
         <li className="text-capitalize">{props.data.description}</li>
         </ul>
-               <div className="row mt-3">
+        <div className="row mt-3">
         <div className="col-6">
-                <img src={props.data.iconUrl} alt={props.data.description}/>
+          <div className="Clear-fix">  
+         <div className="Float-left"> 
+          <WeatherIcon icon={props.data.icon}/>
+          </div>  
         <span className="temperature">{Math.round(props.data.temperature)} </span>
         <span className="unit">ºF</span>      
+      </div>
       </div>
       <div className="col-6">
         <ul>

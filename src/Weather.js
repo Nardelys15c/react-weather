feb550304779a7a26fa2ee5c8b4e2e90721ad28a
Nardelys15c <136908202+Nardelys15c@users.noreply.github.com>
2024-06-21 14:16:14 -0400

    added link for weather icon

--- src/Weather.js
+++ src/Weather.js
@@ -1,11 +1,12 @@
 import React, { useState } from 'react';
-import FormattedDate from "./FormattedDate";
+import WeatherInfo from "./WeatherInfo";
 import "./Weather.css";
 import axios from "axios";
 
 
 export default function Weather(props){
 const [weatherData, setWeatherData]=useState({ready: false});
+const [city, setCity]=useState(props.defaultCity); 
  function handleResponse(response){
     console.log(response.data)
 
@@ -16,57 +17,58 @@ const [weatherData, setWeatherData]=useState({ready: false});
         humidity: response.data.main.humidity,
         date: new Date(response.data.dt * 1000),
         description:response.data.weather[0].description,
-        iconUrl:`https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png`,
+        iconUrl:`https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
         city: response.data.name
     });
  
- 
- }   
+  }  
+  function search(){
+const apiKey="012db25be4dd3739185b772ee4fc78d3";
+let apiUrl=`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`;
+axios.get(apiUrl).then(handleResponse);
+
+  }
+
+  function handleSubmit(event){
+    event.preventDefault();
+    search();
+  
+}
+
+  function handleCityChange(event){
+    setCity(event.target.value);
+   
+
+  }
 
 if (weatherData.ready){
     return(
         <div className="Weather"> 
-        <form>
-            <div className="row">
+        <form onSubmit={handleSubmit}>
+                <div className="row">
                 <div className='col-9'>
-            <input type="search" placeholder="Enter City" className="form-control" autoFocus="on"/>
+            <input 
+            type="search" 
+            placeholder="Enter City" 
+            className="form-control" 
+            autoFocus="on" 
+            onChange={handleCityChange}
+            />
             </div>
             <div className='col-3'>
             <input type="button" value="searh" className="btn btn-primary w-100" />
             </div>
             </div>
         </form>
-        <h1>{weatherData.city}</h1>
-        <ul>
-        <li>
-         <FormattedDate date={weatherData.date}/>
-         </li>
-        <li className="text-capitalize">{weatherData.description}</li>
-        </ul>
-               <div className="row mt-3">
-        <div className="col-6">
-                <img src={weatherData.iconUrl} alt={weatherData.description}/>
-        <span className="temperature">{Math.round(weatherData.temperature)} </span>
-        <span className="unit">ºF</span>      
-      </div>
-      <div className="col-6">
-        <ul>
-           
-            <li>Humidity: {Math.round(weatherData.humidity)}%</li>
-            <li>Wind: {Math.round(weatherData.wind)} mph</li>
-        </ul>
-      </div>
-      </div>
+        <WeatherInfo data={weatherData} />
+       
        </div>
     );
 } else{
-    const apiKey="012db25be4dd3739185b772ee4fc78d3";
+    search();
 
-let apiUrl=`https://api.openweathermap.org/data/2.5/weather?q=${props.defaultCity}&appid=${apiKey}&units=imperial`;
-axios.get(apiUrl).then(handleResponse);
-}
 return "Loading...."
-
+}
 
     
 }

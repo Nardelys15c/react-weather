7332c978f9ab81fe3ed2c6732e76dbe5336d7fe2
Nardelys15c <136908202+Nardelys15c@users.noreply.github.com>
2024-10-14 20:27:27 -0400

    fix daily forecast and added daily forecast

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,27 +1,40 @@
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 
 import "./WeatherForecast.css";
 import Axios from "axios";
 import WeatherForecastDay from "./WeatherForecastDay";
 
 
+
 export default function WeatherForecast(props){
     let [loaded, setLoaded]= useState(false);
     let[forecast, setForecast]=useState(null);
+
+ useEffect(() => {
+    setLoaded(false); 
+ }, [props.coordinates])
 
 function handleResponse(response){
       setForecast(response.data.daily);
       setLoaded(true);
 }
 
+
     
 if (loaded){
     return(
         <div className="WeatherForecast">
         <div className="row">
-            <div className="col">
-           <WeatherForecastDay data={forecast[0]}/>
-        </div>
+            {forecast.map(function (dailyForecast, index){
+                if (index  < 5){
+                    return(
+                        <div className="col" key={index}>
+                        <WeatherForecastDay data={dailyForecast}/>
+                     </div>   
+                    );
+                }
+            })}
+            
         </div>    
         </div>
     );
